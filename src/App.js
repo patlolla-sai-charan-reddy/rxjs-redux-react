fe4612531e96327e7e8cd9cec9a05aea62a84eeb
Reddy, Sai Charan <saicharan.patlolla@fitchratings.com>
2018-08-07 15:00:32 -0400

    next example

--- src/App.js
+++ src/App.js
@@ -3,51 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 // RxJS v6+
 import Rx from '@reactivex/rxjs';
-import { take, map, combineAll } from 'rxjs/operators';
-import { interval } from 'rxjs';
 
 
 
-//emit every 1s, take 2
-const source = interval(1000).pipe(take(2));
-//map each emitted value from source to interval observable that takes 5 values
-const example = source.pipe(
-  map(val => interval(1000).pipe(map(i => `Result (${val}): ${i}`), take(5)))
-);
-/*
-  2 values from source will map to 2 (inner) interval observables that emit every 1s
-  combineAll uses combineLatest strategy, emitting the last value from each
-  whenever either observable emits a value
-*/
-const combined = example.pipe(combineAll());
-/*
-  output:
-  ["Result (0): 0", "Result (1): 0"]
-  ["Result (0): 1", "Result (1): 0"]
-  ["Result (0): 1", "Result (1): 1"]
-  ["Result (0): 2", "Result (1): 1"]
-  ["Result (0): 2", "Result (1): 2"]
-  ["Result (0): 3", "Result (1): 2"]
-  ["Result (0): 3", "Result (1): 3"]
-  ["Result (0): 4", "Result (1): 3"]
-  ["Result (0): 4", "Result (1): 4"]
-*/
-const subscribe = combined.subscribe(val => console.log(val));
-
-console.log("rx", subscribe);
 
 
 // Example 2
-
-const source = Rx.Observable.of(1,2,3,4,5);
-//transparently log values from source with 'do'
-const example = source
-  .do(val => console.log(`BEFORE MAP: ${val}`))
-  .map(val => val + 10)
-  .do(val => console.log(`AFTER MAP: ${val}`));
-//'do' does not transform values
-//output: 11...12...13...14...15
-const subscribe = example.subscribe(val => console.log(val));
+//Create an observable that emits a value every second
+const myInterval = Rx.Observable.interval(1000);
+//Create an observable that emits every time document is clicked
+const bufferBy = Rx.Observable.fromEvent(document, 'click');
+/*
+Collect all values emitted by our interval observable until we click document. This will cause the bufferBy Observable to emit a value, satisfying the buffer. Pass us all collected values since last buffer as an array.
+*/
+const myBufferedInterval = myInterval.buffer(bufferBy);
+//Print values to console
+//ex. output: [1,2,3] ... [4,5,6,7,8]
+const subscribe = myBufferedInterval.subscribe(val => console.log(' Buffered Values:', val));
 
 class App extends Component {
   render() {
