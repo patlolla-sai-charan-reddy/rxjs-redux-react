62df85ed5c52e95b50977aac9476f55cb2c7c6dc
Reddy, Sai Charan <saicharan.patlolla@fitchratings.com>
2018-08-06 22:14:17 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+// RxJS v6+
+import Rx from '@reactivex/rxjs';
+import { take, map, combineAll } from 'rxjs/operators';
+import { interval } from 'rxjs';
+
+
+
+//emit every 1s, take 2
+const source = interval(1000).pipe(take(2));
+//map each emitted value from source to interval observable that takes 5 values
+const example = source.pipe(
+  map(val => interval(1000).pipe(map(i => `Result (${val}): ${i}`), take(5)))
+);
+/*
+  2 values from source will map to 2 (inner) interval observables that emit every 1s
+  combineAll uses combineLatest strategy, emitting the last value from each
+  whenever either observable emits a value
+*/
+const combined = example.pipe(combineAll());
+/*
+  output:
+  ["Result (0): 0", "Result (1): 0"]
+  ["Result (0): 1", "Result (1): 0"]
+  ["Result (0): 1", "Result (1): 1"]
+  ["Result (0): 2", "Result (1): 1"]
+  ["Result (0): 2", "Result (1): 2"]
+  ["Result (0): 3", "Result (1): 2"]
+  ["Result (0): 3", "Result (1): 3"]
+  ["Result (0): 4", "Result (1): 3"]
+  ["Result (0): 4", "Result (1): 4"]
+*/
+const subscribe = combined.subscribe(val => console.log(val));
+
+console.log("rx", subscribe);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
